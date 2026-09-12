56ffabd047f50750b20625b17e447c4e19f5d983
Lucas Coelho <lucasveneno@yandex.com>
2026-09-12 14:40:53 -0300

    chore: update CSP script and image sources and remove trusted types requirement

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const securityHeaders = [
   },
   {
     key: 'Content-Security-Policy',
-    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://ppplayer.com; font-src 'self' data:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; require-trusted-types-for 'script';",
+    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';",
   },
 ]
 
