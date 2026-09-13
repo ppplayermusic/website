a1715d996fcb7cf079c04302dc796054274704bb
Lucas Coelho <lucasveneno@yandex.com>
2026-09-13 11:25:36 -0300

    refactor: extract google domains into a constant for CSP headers

--- proxy.ts
+++ proxy.ts
@@ -15,12 +15,14 @@ export default function middleware(request: NextRequest) {
     
   const trustedTypes = isZaraz ? '' : `require-trusted-types-for 'script';`;
 
-  const cspHeader = `
+    const googleDomains = 'https://*.google.com https://*.google.com.br https://*.google.es https://*.google.co.uk https://*.google.de https://*.google.fr https://*.google.it https://*.google.jp https://*.google.co.jp https://*.google.ca https://*.google.com.au https://*.google.co.in https://*.google.ru https://*.google.cn https://*.google.nl https://*.google.pl https://*.google.ch https://*.google.at https://*.google.be https://*.google.se https://*.google.no https://*.google.dk https://*.google.fi https://*.google.pt https://*.google.ie https://*.google.co.nz https://*.google.com.mx https://*.google.com.ar https://*.google.cl https://*.google.com.co https://*.google.com.pe https://*.google.com.sg https://*.google.com.my https://*.google.co.id https://*.google.co.th https://*.google.com.vn https://*.google.com.ph https://*.google.co.kr https://*.google.com.tw https://*.google.com.hk https://*.google.com.tr https://*.google.ae https://*.google.com.sa https://*.google.co.za';
+
+    const cspHeader = `
     default-src 'self';
     script-src ${scriptSrc};
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.googletagmanager.com;
-    img-src 'self' blob: data: https://ppplayer.com https://flagcdn.com https://www.googletagmanager.com https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.google.com.br https://*.google.es https://*.google-analytics.com https://*.adtrafficquality.google https://*.analytics.google.com;
-    connect-src 'self' https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.google.com https://*.google.es https://*.adtrafficquality.google https://*.googlesyndication.com https://*.doubleclick.net;
+    img-src 'self' blob: data: https://ppplayer.com https://flagcdn.com https://www.googletagmanager.com ${googleDomains} https://*.googlesyndication.com https://*.doubleclick.net https://*.google-analytics.com https://*.adtrafficquality.google https://*.analytics.google.com;
+    connect-src 'self' ${googleDomains} https://*.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.adtrafficquality.google https://*.googlesyndication.com https://*.doubleclick.net;
     font-src 'self' data: https://fonts.gstatic.com;
     frame-src 'self' https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.adtrafficquality.google;
     object-src 'none';
