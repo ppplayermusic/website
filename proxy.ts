ac26742d258d22dc6bb62fc33c8c2e2360393505
Lucas Coelho <lucasveneno@yandex.com>
2026-09-13 07:22:44 -0300

    chore: add adtrafficquality.google to CSP img-src directive

--- proxy.ts
+++ proxy.ts
@@ -11,7 +11,7 @@ export default function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval' 'unsafe-inline' https: http:;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://ppplayer.com https://flagcdn.com https://www.googletagmanager.com https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.google-analytics.com;
+    img-src 'self' blob: data: https://ppplayer.com https://flagcdn.com https://www.googletagmanager.com https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.google-analytics.com https://*.adtrafficquality.google;
     connect-src 'self' https://www.google-analytics.com https://www.google.com https://*.adtrafficquality.google https://*.googlesyndication.com https://*.doubleclick.net;
     font-src 'self' data:;
     frame-src 'self' https://*.googlesyndication.com https://*.doubleclick.net https://*.google.com https://*.adtrafficquality.google;
