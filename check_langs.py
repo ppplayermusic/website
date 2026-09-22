8cf389a09b34007f680d539317fe5f2e79de61f7
Lucas Coelho <lucasveneno@yandex.com>
2026-09-21 22:10:48 -0300

    chore: cleanup and bump version to 2.0.0

--- check_langs.py
+++
@@ -1,5 +0,0 @@
-import json, os, glob
-for f in glob.glob('messages/*.json'):
-    with open(f) as file:
-        data = json.load(file)
-    print(f, data.get('featureGallery', {}).get('videoTitle', 'MISSING'))
